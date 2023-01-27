0424fd2fe335e6da1adc0145b4ccf07fed27d0da
FrazerMichael <mjfrazer1@gmail.com>
2023-01-27 13:42:24 -0500

    update

--- lab1.py
+++ lab1.py
@@ -50,11 +50,6 @@ MF5= {
 
 routers = [MF1, MF2, MF3, MF4, MF5]
 
-for each in routers:
-    net_connect = netmiko.ConnectHandler(**each)
-    output = net_connect.send_command('show ip route')
-    print (output)
-
 #List of IPV4 Networks
 ipv4 = ['12.81.0.4/30', '12.81.4.8/30', '12.81.7.16/30', '12.81.8.20/30', '12.81.9.24/30']
 
@@ -63,14 +58,43 @@ ipv6 = ['2607:f798:12:81:0:4::/127', '2607:f798:12:81:4:8::/127', '2607:f798:12:
 
 
 def assignIPV4(device, subnet, routerNumber, interface):
+    dot = subnet.find('.')
+    startFinal = subnet.find('.', dot + 4)
+    endFinal = subnet.find('/')
+    
+    final = int(subnet[startFinal + 1:endFinal]) + routerNumber
+    ipAddress = subnet[:startFinal + 1] + str(final) + ' 255.255.255.0'
 
+    net_connect = netmiko.ConnectHandler(**device)
+    net_connect.enable()
+    config_ipv4 = ['int ' + interface, 'ip address ' + ipAddress]
+    net_connect.send_config_set(config_ipv4)
 
 
 def assignIPV6(device, subnet, routerNumber, interface):
+    colon = subnet.find(':')
+    endFinal = 
 
 
+#Router MF1 Config
+assignIPV4(MF1, ipv4[0], 1, g0/0)
+assignIPV4(MF1, ipv4[1], 1, g1/0)
 
+#Router MF2 Config
+assignIPV4(MF2, ipv4[0], 2, g0/0)
+assignIPV4(MF2, ipv4[2], 1, g1/0)
 
+#Router MF3 Config
+assignIPV4(MF3, ipv4[2], 2, g1/0)
+assignIPV4(MF3, ipv4[4], 1, g2/0)
+
+#Router MF4 Config
+assignIPV4(MF4, ipv4[1], 2, g1/0)
+assignIPV4(MF4, ipv4[3], 1, g0/0)
+
+#Router MF5 Config
+assignIPV4(MF5, ipv4[3], 2, g0/0)
+assignIPV4(MF5, ipv4[4], 2, g2/0)
 
 
 #net_connect.enable()
