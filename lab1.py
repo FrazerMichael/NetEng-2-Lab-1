2e206b35c3dca6c1de03ce274302048dc64905b8
FrazerMichael <mjfrazer1@gmail.com>
2023-01-27 22:16:23 -0500

    updated ipv6 address function

--- lab1.py
+++ lab1.py
@@ -1,3 +1,10 @@
+#Michael Frazer
+#Network Engineering 2 - Lab 1
+#Learning about netmiko for an OSPF lab.
+#I am feeling very embarrased about how bad this code is.
+#I am looking forward to learning how to make these routers objects. 
+#I apologize if you are reading this "code" maybe... don't.
+
 import netmiko
 
 device = {
@@ -40,7 +47,7 @@ MF4= {
     'secret': 'TELE31831'
     }
 
-MF5= {
+MF5 = {
     'device_type': 'cisco_ios_telnet',
     'host': '192.168.122.50',
     'port': 23,
@@ -70,31 +77,69 @@ def assignIPV4(device, subnet, routerNumber, interface):
     config_ipv4 = ['int ' + interface, 'ip address ' + ipAddress]
     net_connect.send_config_set(config_ipv4)
 
+    return ipAddress
+
 
 def assignIPV6(device, subnet, routerNumber, interface):
-    colon = subnet.find(':')
-    endFinal = 
+    slash = subnet.find('/')
+    if routerNumber == 0:
+    	completeIPV6Address = subnet
+    else:    
+    	slash = subnet.find('/')
+    	completeIPV6Address = subnet[:slash] + str(routerNumber) + subnet[slash:]
+    	
+    net_connect = netmiko.ConnectHandler(**device)
+    net_connect.enable()    
+    config_ipv6 = ['int ' + interface, 'ipv6 address ' + completeIPV6Address]
+    net_connect.send_config_set(config_ipv6)
+
+    return completeIPV6Address
+
+assignIPV6(device, ipv6[0], 0, 'g3/0')
+
+# #Router MF1 Config
+MF1V4G0 = assignIPV4(MF1, ipv4[0], 1, g0/0)
+MF1V4G1 = assignIPV4(MF1, ipv4[1], 1, g1/0)
+MF1V6G0 = assignIPV6(MF1, ipv6[0], 0, g0/0)
+MF1V6G1 = assignIPV6(MF1, ipv6[1], 0, g1/0)
+
+# #Router MF2 Config
+MF2V4G0 = assignIPV4(MF2, ipv4[0], 2, g0/0)
+MF2V4G1 = assignIPV4(MF2, ipv4[2], 1, g1/0)
+MF2V6G0 = assignIPV6(MF2, ipv6[0], 1, g0/0)
+MF2V6G1 = assignIPV6(MF2, ipv6[2], 0, g1/0)
+
+# #Router MF3 Config
+MF3V4G1 = assignIPV4(MF3, ipv4[2], 2, g1/0)
+MF3V4G2 = assignIPV4(MF3, ipv4[4], 1, g2/0)
+MF3V6G1 = assignIPV6(MF3, ipv6[2], 1, g1/0)
+MF3V6G2 = assignIPV6(MF3, ipv6[4], 0, g2/0)
+
+# #Router MF4 Config
+MF4V4G1 = assignIPV4(MF4, ipv4[1], 2, g1/0)
+MF4V4G0 = assignIPV4(MF4, ipv4[3], 1, g0/0)
+MF4V6G1 = assignIPV6(MF4, ipv6[1], 1, g1/0)
+MF4V6G0 = assignIPV6(MF4, ipv6[3], 0, g0/0)
+
+# #Router MF5 Config
+MF5V4G0 = assignIPV4(MF5, ipv4[3], 2, g0/0)
+MF5V4G2 = assignIPV4(MF5, ipv4[4], 2, g2/0)
+MF5V6G0 = assignIPV6(MF5, ipv6[3], 1, g0/0)
+MF5V6G2 = assignIPV6(MF5, ipv6[4], 1, g2/0)
+
+ipv4MF1 = [MF1V4G0, MF1V4G1]
+ipv4MF2 = [MF2V4G0, MF2V4G1]
+ipv4MF3 = [MF3V4G1, MF3V4G2]
+ipv4MF4 = [MF4V4G0, MF4V4G1]
+ipv4MF5 = [MF5V4G0, MF5V4G2]
+
+ipv6MF1 = [MF1V6G0, MF1V6G1]
+ipv6MF2 = [MF2V6G0, MF2V6G1]
+ipv6MF3 = [MF3V6G1, MF3V6G2]
+ipv6MF4 = [MF4V6G0, MF4V6G1]
+ipv6MF5 = [MF5V6G0, MF5V6G2]
 
 
-#Router MF1 Config
-assignIPV4(MF1, ipv4[0], 1, g0/0)
-assignIPV4(MF1, ipv4[1], 1, g1/0)
-
-#Router MF2 Config
-assignIPV4(MF2, ipv4[0], 2, g0/0)
-assignIPV4(MF2, ipv4[2], 1, g1/0)
-
-#Router MF3 Config
-assignIPV4(MF3, ipv4[2], 2, g1/0)
-assignIPV4(MF3, ipv4[4], 1, g2/0)
-
-#Router MF4 Config
-assignIPV4(MF4, ipv4[1], 2, g1/0)
-assignIPV4(MF4, ipv4[3], 1, g0/0)
-
-#Router MF5 Config
-assignIPV4(MF5, ipv4[3], 2, g0/0)
-assignIPV4(MF5, ipv4[4], 2, g2/0)
 
 
 #net_connect.enable()
